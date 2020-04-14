0b03dff14be63b564e9ae7fd1befb13367587ddd
William Mittard <billymittard@ymail.com>
2020-04-14 12:44:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
-def introduction_with_language(name,language)
-  puts "Hi, my name is #{"Dan"} and I am learning to program in #{Ember.js}."
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end 
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
